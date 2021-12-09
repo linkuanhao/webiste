ca145e9d7b389d768010fd11704e0c0629b24dd4
50733146 <50733146@gm.nfu.edu.tw>
2021-12-09 22:17:21 +0800

    add img

---
+++ 統計.py
@@ -0,0 +1,14 @@
+import statistics as stat 
+#統計模組
+data=stat.mean([1,4,5,8])
+#平均值
+print(data)
+data1=stat.median([1,2,3,4,5,6,7])
+#常態分佈
+print(data1)
+data2=stat.stdev([5,6,8,9])
+#標準差
+print(data2)
+data3=stat.median([5,6,8,9])
+#標準差
+print(data3)
